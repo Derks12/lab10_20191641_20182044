f3a340c3d27519e055df11a176f75d5ffcf2627e
Alex <a20191641@pucp.edu.pe>
2023-11-06 21:00:52 -0500

    aaa

--- LAB9_IWEB_BASE/src/main/java/com/example/lab9_base/Dao/DaoSelecciones.java
+++ LAB9_IWEB_BASE/src/main/java/com/example/lab9_base/Dao/DaoSelecciones.java
@@ -2,15 +2,29 @@ package com.example.lab9_base.Dao;
 
 import com.example.lab9_base.Bean.Seleccion;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
-public class DaoSelecciones {
+public class DaoSelecciones extends DaoBase{
     public ArrayList<Seleccion> listarSelecciones() {
 
         ArrayList<Seleccion> selecciones = new ArrayList<>();
-        /*
-        Inserte su código aquí
-        */
+        try (Connection conn = getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("select * from selecciones")){
+
+            while (rs.next()){
+                Seleccion seleccion = new Seleccion();
+                seleccion.setIdSeleccion(rs.getInt(1));
+                seleccion.setNombre(rs.getString(2));
+                seleccion.setTecnico(rs.getString(3));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+
         return selecciones;
     }
 
